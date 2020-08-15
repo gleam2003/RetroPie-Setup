26feb2dd320790658e6a53153a0614cf4a0429ed
gleam2003 <gleam2003@msn.com>
2020-08-15 15:58:34 +0200

    Update system.sh

--- scriptmodules/system.sh
+++ scriptmodules/system.sh
@@ -407,7 +407,7 @@ function get_platform() {
 }
 
 function set_platform_defaults() {
-    __default_opt_flags="-O2"
+    __default_opt_flags="-O3"
 
     # add platform name and 32bit/64bit to platform flags
     __platform_flags=("$__platform" "$(getconf LONG_BIT)bit")
