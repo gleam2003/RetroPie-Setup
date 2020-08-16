636fa8e8679837c3001e409f3c675e38bf75ac7d
gleam2003 <gleam2003@msn.com>
2020-08-16 15:54:37 +0200

    Update install_retroera.sh

--- install_retroera.sh
+++ install_retroera.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 
-# This file is part of The RetroERA Project
-#
-# The RetroERA Project is the legal property of its developers
-#
-
 sudo apt-get update
 sudo apt-get -y install git dialog xmlstarlet joystick
-
 modules=(
     'setup basic_install'
     'autostart enable'
 )
-
 if grep -q "rk3399" /etc/armbian-release; then
-   platform="rk3399" 
+    platform="rk3399"
 fi
 
 for module in "\${modules[@]}"; do
