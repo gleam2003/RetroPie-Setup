b1341f687d76c99456e097ed85ee3ebf66a80e47
gleam2003 <gleam2003@msn.com>
2020-08-16 14:58:17 +0200

    Update and rename install.sh to install_retroera.sh

--- install_retroera.sh
+++ install_retroera.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 sudo apt-get update
 sudo apt-get -y install git dialog xmlstarlet joystick
+
 modules=(
     'setup basic_install'
     'autostart enable'
@@ -8,5 +10,6 @@ modules=(
 for module in "\${modules[@]}"; do
     sudo __platform=rk3399 __nodialog=1 ./retropie_packages.sh \$module
 done
+
 rm -rf tmp
 sudo apt-get clean
