5d2dd657dedd8897115879717601061a0abfda88
gleam2003 <gleam2003@msn.com>
2020-09-04 19:51:00 +0200

    Update lr-mupen64plus.sh

--- scriptmodules/libretrocores/lr-mupen64plus.sh
+++ scriptmodules/libretrocores/lr-mupen64plus.sh
@@ -60,7 +60,7 @@ function build_lr-mupen64plus() {
     elif isPlatform "mali"; then
         params+=(platform="odroid")
     elif isPlatform "armv7-mali"; then
-        params+=(platform="orangepi")
+        params+=(platform="classic_armv7_a7")
     else
         isPlatform "arm" && params+=(WITH_DYNAREC=arm)
         isPlatform "neon" && params+=(HAVE_NEON=1)
